b9314e2efc209a4bea8bffb040457dd9c6fd9bb1
naoru <e195734@ie.u-ryukyu.ac.jp>
2023-02-18 21:18:37 +0900

    左右に分けた

--- background.js
+++ background.js
@@ -2,7 +2,18 @@
 
 const getTabStream = () => {
   chrome.tabCapture.capture({ audio: true, video: false }, (c) => {
-    console.log(c);
+    const audioCtx = new AudioContext();
+    const gainNode = audioCtx.createGain();
+    gainNode.gain.value = -1;
+    const channelSplitterNode = audioCtx.createChannelSplitter();
+    const channelMergerNode = audioCtx.createChannelMerger();
+    const mediaStreamSource = audioCtx.createMediaStreamSource(c);
+
+    splittedStream = mediaStreamSource.connect(channelSplitterNode);
+    splittedStream.connect(channelMergerNode);
+    //splittedStream.connect(gainNode).connect(channelMergerNode);
+
+    channelMergerNode.connect(audioCtx.destination);
   });
 }
 chrome.browserAction.onClicked.addListener(() => {
